c328d68869902ddd82146bc87d5a7b16d8dd33b1
Kevin wu | Biondo <kevin.wu@bindo.com>
2022-02-24 23:42:42 +0800

    init

---
+++ golang1.go
@@ -0,0 +1,24 @@
+1.
+package main
+
+import "fmt"
+
+func main(){
+defer_call()
+}
+
+func defer_call(){
+defer func(){fmt.Println("打印前")}()
+defer func(){fmt.Println("打印中")}()
+defer func(){fmt.Println("打印后")}()
+
+panic("触发异常")
+}
+/*
+打印后
+打印中
+打印前
+panic: 触发异常
+ */
+
+2.
